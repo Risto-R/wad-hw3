be39d64463414a3861dffaca84e2dea588c3c3d0
Risto-R <rahulan.risto@gmail.com>
2019-11-08 15:40:18 +0200

    Task 1, Task 2

---
+++ src/components/MainContent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="content">
+            <div id="profile-container" class="tab" v-bind:class="{ active: active }">
+                <Profile :user="user"></Profile>
+            </div>
+            <div id="courses-container" class="tab" v-bind:class="{ active: !active }">
+                <Courses :courses="courses"></Courses>
+            </div>
+        </div>
+        <div class="controls">
+            <button id="profile-button" class="pill" v-bind:class="{'is-active': active === true,active: active}"
+                    name="button" @click="active=true">Profile
+            </button>
+            <button id="courses-button" class="pill" :class="{'is-active': active === false,active: !active}"
+                    name="button" @click="active=false">Courses
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Profile from '../components/Profile';
+    import Courses from "../components/Courses";
+    import User from "../models/User";
+    import Course from "../models/Course";
+
+    export default {
+        name: "MainContent",
+        components: {Profile, Courses},
+        data: () => {
+            return {
+                active: true,
+                user: new User("John", "Doe", "11/10/1990", "Faculty of Science and Technology", 2.5),
+                courses: [new Course("Web Application Development", 3, 100),
+                    new Course("Introduction to Data Science", 3, 91),
+                    new Course("Software Engineering", 3, 87),
+                    new Course("Algorithms and data structures", 3, 85)]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <div id="profile">
+        <div class="avatar">
+            <img src="../assets/me.png" id="picture" alt="My picture">
+        </div>
+        <div class="info">
+            <ul>
+                <li id="name">{{user.firstname}} {{user.lastname}}</li>
+                <li id="birthdate">{{user.birthdate}}</li>
+                <li id="faculty">{{user.faculty}}</li>
+            </ul>
+        </div>
+        <div id="gpa">
+            <strong>{{user.gpa}}</strong>
+        </div>
+        <div class="clear-fix"></div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Profile",
+        props: {
+            user: Object
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
